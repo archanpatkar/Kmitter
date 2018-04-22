85f15b37c38641d6b3d45dc1ae6c479e5e5c9386
Archan Patkar <archanpatkar@gmail.com>
2018-04-22 12:30:45 +0530

    Update

--- Emitter.kt
+++ Emitter.kt
@@ -1,4 +1,11 @@
+package com.archanpatkar.emitter;
+
+import java.util.*;
+
 fun main(args: Array<String>)
 {
   println("Hello World!");
+  println("50 + 60 = ${sum(50,60)}")
 }
+
+fun sum(a: Int , b:Int) = a + b
