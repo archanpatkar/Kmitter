a6ac280fd149880562dd0964502657111da85aee
Archan Patkar <archanpatkar@gmail.com>
2018-05-25 18:22:08 +0530

    Update

--- FunctionWrapper.kt
+++ FunctionWrapper.kt
@@ -4,10 +4,12 @@ public class FunctionWrapper
 {
   val function: (params:Array<Any>) -> Unit;
   val times: Int;
+  var count: Int;
 
   constructor(function:(params:Array<Any>) -> Unit,times:Int = 1)
   {
     this.function = function;
     this.times = times;
+    this.count = 0;
   }
 }
