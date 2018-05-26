8cc8dfcfa9fac1a70b637ab1df9f01c7da08b38f
Archan Patkar <archanpatkar@gmail.com>
2018-05-27 00:35:52 +0530

    Update

--- Emitter.kt
+++ Emitter.kt
@@ -24,11 +24,11 @@ interface Emitter
  TODO: Adding support for number of times the handler was executed [Done]
  TODO: Adding support for chaining by returning this [Done]
  TODO: Adding support for Single Event Instance Handlers [Done]
- TODO: Adding support for Regular Expressions [Current]
- TODO: Adding support for Universal Handler [Current]
+ TODO: Adding support for Regular Expressions [Mostly Done]
+ TODO: Adding support for Universal Handler [Mostly Done]
  TODO: Adding support for Namespaces [Next]
  TODO: Adding support for param number control per event and per handler [Next]
  TODO: Adding support for Interception between handlers and before and after the event
  TODO: Adding support for a Reactive Property named Promitter with ready hooks for
-       before,changed,after in addition to interceptors and also a Async Variation
+       before,changed,after in addition to interceptors and also an Async Variation
  */
