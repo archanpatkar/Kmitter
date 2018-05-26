0c4aaf1fdf7775b1af33d3b9c314b37b23761b16
Archan Patkar <archanpatkar@gmail.com>
2018-05-27 01:01:38 +0530

    Update

--- PatternWrapper.kt
+++ PatternWrapper.kt
@@ -2,14 +2,12 @@ package com.archanpatkar.emitter;
 
 import kotlin.text.Regex;
 
-public class PatternWrapper
+public class PatternWrapper: FunctionWrapper
 {
   val regex: Regex;
-  var function: FunctionWrapper;
 
-  constructor(pattern: Regex,function:(params:Array<Any>) -> Unit,times:Int = 1)
+  constructor(pattern: Regex,function:(params:Array<Any>) -> Unit,times:Int = 1): super(function,times)
   {
     this.regex = pattern;
-    this.function = FunctionWrapper(function,times);
   }
 }
